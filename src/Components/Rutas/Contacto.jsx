3be11163bea8b7ab3874a90b3413c0caaf2c3233
leo3dx <leo3dxl@gmail.com>
2021-11-05 14:15:48 -0500

    correción del componente contacto

--- src/Components/Rutas/Contacto.jsx
+++ src/Components/Rutas/Contacto.jsx
@@ -1,13 +1,38 @@
 import React, {Component} from "react";
+import {init} from "emailjs-com";
+import emailjs from "emailjs-com";
+
 class Contacto extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
     }
+    enviar(e){
+      e.preventDefault();
+      init("user_qMQQFb77OfTUyGqNOy7My");
+      let tempParams = {
+        from_name: document.getElementById("from_name").value,
+        to_email: document.getElementById("to_email").value,
+        to_date: document.getElementById("to_date").value,
+        to_phone: document.getElementById("to_phone").value,
+        mensaje: document.getElementById("mensaje").value,
+        to_subject: document.getElementById("to_subject").value,
+    };
+    if(tempParams.to_email === "" && tempParams.to_phone === ""){
+        alert("Ingrese los datos")
+    }else{
+        emailjs.send("gmail","template_yg4gq9h",tempParams)
+        .then(function(response){
+        console.log("success",response.status);
+    });
+    this.props.history.push('/')
+    }
+
+    }
     render() { 
         return (
           <>
-            <form>
+            <form onSubmit={this.enviar.bind(this)}>
               <section class="container mt-5 mb-5">
                     <div>
                       <h1>Contáctenos</h1>
@@ -15,23 +40,23 @@ class Contacto extends Component {
                     <form action="">
                         <div class="mb-3">
                             <label for="exampleFormControlInput1" class="form-label">Asunto</label>
-                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Ingresa el asunto" required="required"/>
+                            <input type="text" class="form-control" id="to_subject" placeholder="Ingresa el asunto" required="required"/>
                           </div>
                           <div class="mb-3">
                             <label for="exampleFormControlInput1" class="form-label">Nombre completo</label>
-                            <input type="text" class="form-control" id="exampleFormControlInput2" placeholder="Ingresa tu nombre" pattern="[A-Za-z ]{1,50}" required="required"/>
+                            <input type="text" class="form-control" id="from_name" placeholder="Ingresa tu nombre" pattern="[A-Za-z ]{1,50}" required="required"/>
                           </div>
                           <div class="mb-3">
                             <label for="exampleFormControlInput1" class="form-label">Telefono</label>
-                            <input type="numer" class="form-control" id="exampleFormControlInput2" placeholder="Ingresa tu numero" required="required"/>
+                            <input type="numer" class="form-control" id="to_phone" placeholder="Ingresa tu numero" required="required"/>
                           </div>
                           <div class="mb-3">
                             <label for="exampleFormControlInput1" class="form-label">Fecha</label>
-                            <input type="date" class="form-control" id="exampleFormControlInput2" required="required"/>
+                            <input type="date" class="form-control" id="to_date" required="required"/>
                           </div>
                           <div class="mb-3">
                             <label for="exampleFormControlInput1" class="form-label">Correo electronico</label>
-                            <input type="email" class="form-control" id="exampleFormControlInput2" placeholder="Ingresa su correo electronico" pattern="{1,20}" required="required"/>
+                            <input type="email" class="form-control" id="to_email" placeholder="Ingresa su correo electronico" pattern="{1,20}" required="required"/>
                           </div>
                           <div class="mb-3">
                             <label for="exampleFormControlInput1" class="form-label">Servicios</label>
@@ -48,13 +73,13 @@ class Contacto extends Component {
                           </div>
                           <div class="mb-3">
                             <label for="exampleFormControlTextarea1" class="form-label">Descripción del servicio</label>
-                            <textarea class="form-control" placeholder="Ingrese la descripción del servicio"required="required" id="exampleFormControlTextarea1" minlength="10" maxlength="250" rows="3"></textarea>
+                            <textarea class="form-control" placeholder="Ingrese la descripción del servicio"required="required" id="mensaje" minlength="10" maxlength="250" rows="3"></textarea>
                         </div>
                         <div class="mb-3 form-check">
                           <input type="checkbox" required class="form-check-input" id="exampleCheck1"/>
                           <label class="form-check-label" for="exampleCheck1">¿Acepta el tratamiento de datos personales?</label>
                         </div>
-                        <button type="submit" class="btn btn-danger">Enviar</button>
+                        <input type="submit" onClick={this.enviar.bind(this)} class="btn btn-danger" value="Enviar"/>
                     </form>
                   </section>
             </form>
